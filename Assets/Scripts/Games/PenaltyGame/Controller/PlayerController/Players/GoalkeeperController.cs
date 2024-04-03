fe4e04497266f17d1e8b758c80bf253e2b8cec01
annajcy <2821872892@qq.com>
2024-04-03 18:22:12 +0800

    enhanced goalkeeper ai

--- Assets/Scripts/Games/PenaltyGame/Controller/PlayerController/Players/GoalkeeperController.cs
+++ Assets/Scripts/Games/PenaltyGame/Controller/PlayerController/Players/GoalkeeperController.cs
@@ -4,6 +4,8 @@ using Random = UnityEngine.Random;
 
 public class GoalkeeperController : PlayerController
 {
+    public Transform ballTransform;
+    public Transform goalTransform;
     private void OnEnable()
     {
         Respawn();
@@ -13,7 +15,89 @@ public class GoalkeeperController : PlayerController
     public GoalkeeperAction GetPredictedAction(Vector3 speed)
     {
         animator.speed = Random.Range(1.0f, 1.5f);
-        return (GoalkeeperAction)Random.Range(1, 9);
+        Ray ray = new Ray(ballTransform.position, speed);
+        if (Physics.Raycast(ray, out RaycastHit rayCastHit))
+        {
+            if (rayCastHit.collider.gameObject.name == "ScoreSurface")
+            {
+                Vector3 position = rayCastHit.point;
+                Vector3 saveVector = position - goalTransform.position;
+                if (saveVector.x < 1.5 && saveVector.x > -1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.CenterDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.CenterMid;
+                    else
+                        return GoalkeeperAction.CenterUp;
+
+                }
+                else if (saveVector.x <= -1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.LeftDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.LeftMid;
+                    else
+                        return GoalkeeperAction.LeftUp;
+                }
+                else if (saveVector.x >= 1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.RightDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.RightMid;
+                    else
+                        return GoalkeeperAction.RightUp;
+                }
+            }
+        }
+        return 0;
+    }
+
+    [ContextMenu(nameof(GetPredictedAction))]
+    public GoalkeeperAction GetPredictedAction()
+    {
+        Vector3 speed = new Vector3(0.2f, 0, -1);
+        animator.speed = Random.Range(1.0f, 1.5f);
+        Ray ray = new Ray(ballTransform.position, speed);
+        if (Physics.Raycast(ray, out RaycastHit rayCastHit))
+        {
+            if (rayCastHit.collider.gameObject.name == "ScoreSurface")
+            {
+                Vector3 position = rayCastHit.point;
+                Vector3 saveVector = position - goalTransform.position;
+                if (saveVector.x < 1.5 && saveVector.x > -1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.CenterDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.CenterMid;
+                    else
+                        return GoalkeeperAction.CenterUp;
+
+                }
+                else if (saveVector.x <= -1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.LeftDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.LeftMid;
+                    else
+                        return GoalkeeperAction.LeftUp;
+                }
+                else if (saveVector.x >= 1.5)
+                {
+                    if (saveVector.y < 0.7)
+                        return GoalkeeperAction.RightDown;
+                    else if (saveVector.y >= 0.7 && saveVector.y <= 1.7)
+                        return GoalkeeperAction.RightMid;
+                    else
+                        return GoalkeeperAction.RightUp;
+                }
+            }
+        }
+        return 0;
     }
 
     public void SwitchPose(GoalkeeperAction goalkeeperAction)
